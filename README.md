404bece48e022158439c83887b6db274d7173833
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-01-13 11:22:31 -0300

    inciando a criação do butão

---
+++ README.md
@@ -0,0 +1,21 @@
+# Portfólio Pessoal
+
+## Linguagens e ferramentas  usadas
+<div >
+    <img align="center" alt="css" src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white">
+    <img align="center" alt="html" src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white">
+    <img align="center" alt="javascript" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+    <img align="center" alt="vscode" src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white">
+</div><br/>
+
+![Portfólio](https://github.com/Marcos-Gabriell/Portif-lio-/blob/main/img/projec.png)
+
+Bem-vindo ao meu portfólio pessoal, onde você pode conhecer um pouco mais sobre mim, minhas habilidade, meus serviços, meus projetos e como entrar em contato comigo.
+
+## Acesse o Projeto
+
+Você pode visualizar meu portfólio completo acessando o seguinte link: [Portfólio Marcos Gabriel](https://marcosgabriel.vercel.app/)
+
+Sinta-se à vontade para explorar e conhecer mais sobre meu trabalho. Se você tiver alguma dúvida ou quiser entrar em contato, não hesite em me enviar uma mensagem através das informações de contato disponíveis no site.
+
+Obrigado por visitar meu portfólio!
---
+++ script.js
@@ -0,0 +1,82 @@
+AOS.init();
+
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+// Adicione um evento de clique ao ícone do menu
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("active"); // Alternar a classe 'active' na navbar
+  toggleMenuIcon();
+});
+function toggleMenuIcon() {
+  const menuIcon = document.querySelector("#menu-icon");
+  if (menuIcon.classList.contains("fa-bars")) {
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-x");
+  } else {
+    menuIcon.classList.remove("fa-x");
+    menuIcon.classList.add("fa-bars");
+  }
+}
+
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+window.onscroll = () => {
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+    toggleMenuIcon(); // Certifique-se de que o ícone do menu seja alterado quando o menu for fechado
+  }
+};
+
+// Dark Mode / light mode (seu código JavaScript atual permanece o mesmo aqui)
+darkmode.onclick = () => {
+  const darkModeIcon = document.querySelector("#dark-mode-icon");
+
+  if (document.body.classList.contains("active")) {
+    document.body.classList.remove("active");
+    header.classList.remove("dark-header");
+    darkModeIcon.classList.remove("fas", "fa-sun");
+    darkModeIcon.classList.add("fas", "fa-moon");
+  } else {
+    document.body.classList.add("active");
+    header.classList.add("dark-header");
+    darkModeIcon.classList.remove("fas", "fa-moon");
+    darkModeIcon.classList.add("fas", "fa-sun");
+  }
+};
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact-form");
+
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Impede o envio padrão do formulário
+
+      // Coleta os dados do formulário
+      const name = form.querySelector('[name="name"]').value;
+      const email = form.querySelector('[name="email"]').value;
+      const message = form.querySelector('[name="message"]').value;
+
+      // Aqui você pode adicionar lógica para enviar os dados para um servidor ou fazer qualquer outra coisa que desejar com eles
+
+      // Exibe uma mensagem de sucesso
+      alert("Mensagem enviada com sucesso!");
+
+      // Limpa o formulário
+      form.reset();
+    });
+  });
+
+  function mostrarDetalhes(id) {
+    const detalhes = document.getElementById(id);
+    if (detalhes.style.display === "block") {
+        detalhes.style.display = "none";
+    } else {
+        detalhes.style.display = "block";
+    }
+}
